f733f029f398045bb6366899b3aaf123a030edf0
abdiraimov-otabek <otabekabdiraimovv@gmail.com>
2025-02-06 17:22:34 +0500

    env file updated

--- data/config.py
+++ data/config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Bot Token
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 
 
-DB_USER = env.str("DB_USER")
-DB_PASS = env.str("DB_PASS")
-DB_NAME = env.str("DB_NAME")
-DB_HOST = env.str("DB_HOST")
+DB_USER = env.str("PGUSER")
+DB_PASS = env.str("POSTGRES_PASSWORD")
+DB_NAME = env.str("POSTGRES_DB")
+DB_HOST = env.str("PGHOST")
