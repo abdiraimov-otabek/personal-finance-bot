f05292b369abfe0c1f4ce49ad3edcd15999eeb6c
abdiraimov-otabek <otabekabdiraimovv@gmail.com>
2025-02-11 17:38:37 +0500

    some updates

--- data/config.py
+++ data/config.py
@@ -1,4 +1,5 @@
 from environs import Env
+from urllib.parse import urlparse
 
 # environs kutubxonasidan foydalanish
 env = Env()
@@ -6,10 +7,17 @@ env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot Token
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+ADMINS = env.list("ADMINS")  # Adminlar ro'yxati
+
+DATABASE_URL = env.str("DATABASE_URL")
 
 
-DB_USER = env.str("DB_USER")
-DB_PASS = env.str("DB_PASS")
-DB_NAME = env.str("DB_NAME")
-DB_HOST = env.str("DB_HOST")
+db_url = urlparse(DATABASE_URL)
+DB_USER = db_url.username
+DB_PASS = db_url.password
+DB_NAME = db_url.path[
+    1:
+]
+DB_HOST = db_url.hostname
+DB_PORT = db_url.port
+
