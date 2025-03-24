42ed220919affdddd86a5360286a82c592c41207
abdiraimov-otabek <otabekabdiraimovv@gmail.com>
2025-03-24 17:04:38 +0500

    subscription

--- loader.py
+++ loader.py
@@ -1,9 +1,7 @@
 from aiogram import Bot
-from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-
-from data.config import BOT_TOKEN
 from utils.db.postgres import Database
+from data.config import BOT_TOKEN
 
 db = Database()
-bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
